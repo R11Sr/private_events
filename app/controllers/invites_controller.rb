e7dd9d4860e75c6bde1ace03e081cbfb08b242fa
r11Sr <rs_burner2@outlook.com>
2021-08-20 00:35:30 -0500

    removed unused resources for userevents from routes, views and controller. Also added the functionality that disabled the creation of duplicate invites/RSVP

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,17 +4,26 @@ class InvitesController < ApplicationController
 
     
     def new
-        # @event = Event.find(params[:event])
         @invite = Invite.new
         @invite.event_id = @event.id
         @invite.attendee_id = current_user.id
-        if @invite.save
-            flash[:notice] = "#{current_user.name} invited to attend Event at #{@event.location}!"
+
+        #test to see if the User is already and attendee to that event
+        if Invite.where('event_id =:event AND attendee_id = :attendee',{event: @event.id, attendee: current_user.id}).any?
+            flash[:alert] = "You are already invited to this Event"
             redirect_to users_invitedevents_path
+
         else
-            flash[:alert] = "Failed to accept invite"
-            redirect_to root_path
+            #if the user has not been invited. Save the invitation
+            if @invite.save
+                flash[:notice] = "#{current_user.name} invited to attend Event at #{@event.location}!"
+                redirect_to users_invitedevents_path
+            else
+                flash[:alert] = "Failed to accept invite"
+                redirect_to root_path
+            end
         end
+
     end 
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => {registrations: 'registrations'}# Important to have this line added to the routes  
   resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'users/events', to: 'userevents#displayhostedevents'
-  get 'users/all', to: 'userevents#allUsers'
-  get 'users/invitemanager', to: 'userevents#EventsToAttend'
-  get 'users/attend', to: 'userevents#attend'
-  get 'users/invitedevents', to: 'userevents#associatedEvents'
   resources :userevents
-
   resources :invites
+  get 'users/invitedevents', to: 'userevents#associatedEvents'
 
 
   root "events#index"
